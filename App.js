35db3ee80502cb3ce3fa28717959e59aee7d8551
Way05 <130930847+Way05@users.noreply.github.com>
2023-08-06 20:30:50 -0400

    untested code

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.topText}>
       <Text>NO WAY IT WORKS</Text>
       <StatusBar style="auto" />
-    </View>
+
+      <View style={styles.activityContainer}>
+        //add activities here through code
+      </View>
+
+      <button />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  topText: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+  activityContainer: {
+    height: "80%",
+    width: "auto",
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  addButton: {},
 });
