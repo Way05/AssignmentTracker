25ded4fe8619078af517b7c5c156c0141d6b3eb1
Way05 <wtan235@gmail.com>
2023-08-18 17:28:47 -0400

    forgot to commit new theme file

---
+++ src/app-data/themes.js
@@ -0,0 +1,11 @@
+export default THEMES = {
+  DARK: {
+    background: "black",
+    modal: "black",
+    shadow: "black",
+    activities: "black",
+    tasks: "black",
+    buttons: "white",
+    text: "white",
+  },
+};
