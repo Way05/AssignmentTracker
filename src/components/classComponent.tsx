5d2df11ed1fc560e3fd2f0d4b75a1adcb36e593a
Way05 <130930847+Way05@users.noreply.github.com>
2023-08-19 09:40:57 -0400

    components converted to tsx

---
+++ src/components/classComponent.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export type Props = {
+  name: string;
+  date: number;
+};
+
+const ClassDisplay: React.FC<Props> = ({ name, date }) => {
+  const dateString = (days: number) =>
+    days > 0 ? `${days} days left` : `${days} days overdue!`;
+
+  return (
+    <View style={styles.tHolder}>
+      <Text style={styles.t}>{name}</Text>
+      <Text style={styles.t}>{dateString(date)}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tHolder: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  t: {
+    width: "50%",
+    color: "white",
+    textAlign: "center",
+  },
+});
+
+export default ClassDisplay;
