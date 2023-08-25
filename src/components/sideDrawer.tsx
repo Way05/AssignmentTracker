66a4dd0b20c522a981106e179f53b69bf9adce35
Way05 <130930847+Way05@users.noreply.github.com>
2023-08-24 21:42:34 -0400

    some draw file initialized

---
+++ src/components/sideDrawer.tsx
@@ -0,0 +1,7 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigator();
+
+function drawer() {
+  return <Drawer.Navigator></Drawer.Navigator>;
+}
