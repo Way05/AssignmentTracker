cc2dffa6951549684af7968cf62ae5d34ff467cd
Way05 <wtan235@gmail.com>
2023-08-31 20:34:50 -0400

    initial tests for writeFile

---
+++ src/app-data/handleData.js
@@ -0,0 +1,18 @@
+import ClassData from "./classesOBJ";
+
+var RNFS = require("react-native-fs");
+
+var path = RNFS.DocumentDirectoryPath + "src/app-data/text-data";
+var classesTxt = "classes.txt";
+var themesTxt = "themes.txt";
+
+function writeFile(filePath, data) {
+  RNFS.writeFile(filePath, data, "utf8")
+    .then((success) => {
+      console.log("data saved");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+writeFile(path + classesTxt, ClassData);
